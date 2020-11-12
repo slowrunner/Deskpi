a3b04b08486679c359b9fae5c6d1e82d23cdd12f
slowrunner <slowrunner@users.noreply.github.com>
2020-11-12 15:36:59 -0500

    .

---
+++ status.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+#
+# loop printing boinc status, proc temp, freq and throttled status
+#
+# 0x50000 means throttling occurred, under-voltage occurred
+# 0x50005 means throttled now, under-voltage now
+#
+while true; do echo -n `date +"%A %D"`; uptime; vcgencmd measure_temp && vcgencmd measure_clock arm && vcgencmd get_throttled; echo -e "vout:\c " && lifepo4wered-cli get vout; boinccmd --get_simple_gui_info | grep fraction; sleep 30; echo ' '; done
